921892a9228d8b132a2be5c4b9a9cfc66c59a760
corikiyomi <corikiyomi@gmail.com>
2024-02-21 15:31:13 -0700

    Use PIL, Image and ImageFilter to manipulate images

--- images.py
+++ images.py
@@ -4,8 +4,10 @@ img = Image.open('./Pokedex/pikachu.jpg')
 
 # filtered_img = img.filter(ImageFilter.SMOOTH)
 filtered_img = img.convert('L')
+box = (100, 100, 400, 400)
+region = filtered_img.crop(box)
 # crooked = filtered_img.rotate(90)
 resize = filtered_img.resize((300, 300))
-resize.save("grey.png", 'png')
+region.save("grey.png", 'png')
 # filtered_img.show() # opens a Preview window with image
 
