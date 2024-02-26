3c270a43939cace817077b2a4eae2b1524d50060
corikiyomi <corikiyomi@gmail.com>
2024-02-26 13:43:08 -0700

    Used sys, os and PIL to create a simple JPG to PNG image converter

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -8,9 +8,21 @@ import os
 from PIL import Image
 
 # grab first and second argument
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+print(image_folder, output_folder)
 
 # check if new/ exists, if not create
-
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+    
 # loop through Pokedex
+for filename in os.listdir(image_folder):
+    img = Image.open(f'{image_folder}{filename}')
+    clean_name = os.path.splitext(filename)[0]
+    img.save(f'{output_folder}{clean_name}.png', 'png')
+    print('all done!')
+
 # convert images to png
 # save to the new folder
