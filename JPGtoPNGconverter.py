afd98a212d039a34b55526c02e3c4c1cc1c80ae9
corikiyomi <corikiyomi@gmail.com>
2024-02-26 13:22:11 -0700

    Create JPG to PNG converter

---
+++ JPGtoPNGconverter.py
@@ -0,0 +1,16 @@
+# takes two args from user input 
+    # first arg is current image folder
+    # second arg is the new image folder that gets created
+    # if you run script: python3 JPGtoPNGconverter.py Pokedex/ new/
+
+import sys
+import os
+from PIL import Image
+
+# grab first and second argument
+
+# check if new/ exists, if not create
+
+# loop through Pokedex
+# convert images to png
+# save to the new folder
--- images.py
+++ images.py
@@ -1,13 +1,16 @@
 from PIL import Image, ImageFilter
 
-img = Image.open('./Pokedex/pikachu.jpg')
+img = Image.open('./astro.jpg')
+
+img.thumbnail((400, 400))
+img.save('thumbnail.jpg')
 
 # filtered_img = img.filter(ImageFilter.SMOOTH)
-filtered_img = img.convert('L')
-box = (100, 100, 400, 400)
-region = filtered_img.crop(box)
+# filtered_img = img.convert('L')
+# box = (100, 100, 400, 400)
+# region = filtered_img.crop(box)
 # crooked = filtered_img.rotate(90)
-resize = filtered_img.resize((300, 300))
-region.save("grey.png", 'png')
+# resize = filtered_img.resize((300, 300))
+# region.save("grey.png", 'png')
 # filtered_img.show() # opens a Preview window with image
 
